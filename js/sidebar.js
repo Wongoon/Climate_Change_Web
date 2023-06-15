0d8439cdc6f81c47a408e1bdfd61189f4d6a83d5
Wongoon <rmsgh64@gmail.com>
2023-06-15 15:35:34 +0900

    code update

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,24 @@
-let arrow = document.querySelector('.arrow');
-arrow.addEventListener('click', (e) => {
-    let arrowParent = e.target.parentElement.parentElement;
-    arrowParent.classList.toggle('showMenu');
-});
-
+let lists = document.querySelectorAll('.menu-links li');
 let sidebar = document.querySelector('.sidebar');
 let toggle = document.querySelector('.toggle');
+let contents = document.querySelectorAll('.content');
+
+lists.forEach(list => {
+    list.addEventListener('click', function(){
+        let tab = list.getAttribute('data-tab');
+        let content = document.getElementById(tab);
+
+        lists.forEach(list => {
+            list.classList.remove('current');
+        });
+        contents.forEach(content => {
+            content.classList.remove('current');
+        });
+
+        list.classList.add('current');
+        content.classList.add('current');
+    });
+});
 
 toggle.addEventListener('click', function(){
     sidebar.classList.toggle('close');
